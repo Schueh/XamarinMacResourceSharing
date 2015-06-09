4f98bbf059a16b9b738db0704e7dca77b9644d47
Manuel Sidler <manuel.sidler@gmx.ch>
2015-06-09 19:09:14 +0200

    add resources

---
+++ src/ResourceSharing.Localization/LocalizedResources.cs
@@ -0,0 +1,12 @@
+﻿namespace ResourceSharing.Localization
+{
+    public class LocalizedResources
+    {
+        private static readonly AppResources _resources;
+
+        public static AppResources Resources
+        {
+            get { return _resources; }
+        }
+    }
+}
